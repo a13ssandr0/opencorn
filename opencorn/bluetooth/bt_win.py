b38f10445f1c0d79f9c05599dcad7f1cb08f48bd
Alessandro Campolo <campoloalex@gmail.com>
2024-05-01 20:05:21 +0200

    Device mac address retrieval fixed in windows bluetooth module

--- opencorn/bluetooth/bt_win.py
+++ opencorn/bluetooth/bt_win.py
@@ -2,13 +2,9 @@ import os
 if os.name != 'nt':
     raise ImportError(f"Windows module cannot be imported on your system ({os.name})")
 
-from re import compile
 import serial.tools.list_ports as list_ports
 import win32com.client as win32
 
-
-## 'BTHENUM\\{00001101-0000-1000-8000-00805F9B34FB}_LOCALMFG&0002\\7&4ABA4C4&0&AABBCCDDEEFF_C00000000'
-hwid_rex = compile(r'BTHENUM\\{00001101-0000-1000-8000-00805F9B34FB}_LOCALMFG&[\dA-F]{4}\\[\dA-F]&[\dA-F]{7}&[\dA-F]&([\dA-F]{12})_[\dA-F]+')
 
 wmi_service = win32.GetObject("winmgmts:\\\\.\\root\\cimv2")
 
@@ -17,9 +13,10 @@ def get_paired_devices(filter_names: bool = True) -> list[tuple[str, str, str]]:
     serial_devs = []
 
     for port in list_ports.comports():
-        m = hwid_rex.match(port.hwid)
-        if m and m[1] != '000000000000':
-            serial_devs.append(('', m[1], port.device))
+        ## 'BTHENUM\\{00001101-0000-1000-8000-00805F9B34FB}_LOCALMFG&0002\\7&4ABA4C4&0&AABBCCDDEEFF_C00000000'
+        mac = port.hwid.split('&')[-1][:12]
+        if mac != '000000000000':
+            serial_devs.append(('', mac, port.device))
 
     serial_bt_devs = []
 
